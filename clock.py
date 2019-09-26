307640c0055aa554895c8bcf337208099024966e
Jim Harter <james.r.harter.ii@gmail.com>
2019-09-25 19:49:34 -0500

    added getTime, rough out of getDifference

--- clock.py
+++ clock.py
@@ -1,13 +1,35 @@
 IN_FILE = "clock.in"
 CLOCK_OFFSET = 100
 
-#returns {before: 0000, after: 0000}
-def getTimes(inFileStr)
+#returns {before: {hour: 00, minute: 00}, after: {hour: 00, minute: 00}}
+def getTimes(inFileStr):
+    with open(inFileStr, 'r') as inFile:
+        beforeTimeList = inFile.readline.strip().split(":")
+        afterTimeList = inFile.readline.strip().split(":")
+
+        return {
+            "before": {
+                "hour": beforeTimeList[0],
+                "minute": beforeTimeList[1]
+            },
+            "after": {
+                "hour": afterTimeList[0],
+                "minute": afterTimeList[1]
+            }
+        }
 
 #time: 0000, origin: {"x","y"}
 def drawClock(time, origin)
 
-def getDifference(beforeTime, afterTime)
+#return 0000
+def getDifference(beforeTime, afterTime):
+    minutesDifference = afterTime["minute"] - beforeTime["minute"]
+    hoursDifference = afterTime["hour"] - beforeTime["hour"]
+
+    #if minutes is neg, add 60 and subtract from hours
+    #if hours is neg, add 24
+
+    return 0
 
 times = getTimes(IN_FILE)
 
